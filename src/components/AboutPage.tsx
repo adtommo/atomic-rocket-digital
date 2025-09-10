639464661d0c42359832bbdf4790766fbf71228a
Adam T <adam.tommo@hotmail.co.uk>
2025-09-10 21:51:18 +0100

    fix build errors

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
